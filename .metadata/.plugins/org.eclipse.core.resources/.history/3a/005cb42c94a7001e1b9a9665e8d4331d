222b3990cbdb1c2a6b145e99e5e8def82db64a63
Gerson Santos <gersones@outlook.com>
2023-12-31 19:04:00 -0300

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3a/005cb42c94a7001e1b9a9665e8d4331d
@@ -0,0 +1,10 @@
+package contas;
+
+public class Banco {
+	public static void main ( String arg [ ] ){
+		Conta minhaConta = new Conta ( "Bob", 300.50, 02345, 1234 );
+		minhaConta.deposita (100);
+		minhaConta.saca (50);
+		minhaConta.saldo();
+		}
+}
---
+++ Sum/Sum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int  num1, num2;
+		 System.out.print("Digite o primeiro número: ");
+		 num1 = sc.nextInt();
+		 System.out.print("Digite o segundo número: ");
+		 num2 = sc.nextInt();
+		 System.out.println("Soma = " + (num1 + num2)); 
+	}
+}
+
