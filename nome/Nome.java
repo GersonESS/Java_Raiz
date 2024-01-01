3a0b37592b1dbf7badc7b73ad822e1e57c0acfd4
Gerson Santos <gersones@outlook.com>
2024-01-01 14:23:56 -0300

    a

--- nome/Nome.java
+++ nome/Nome.java
@@ -1,13 +1,14 @@
+
 import java.util.Scanner;
 public class Nome {
-
     public static void main(String[] args) {
-
         Scanner ler = new Scanner(System.in);
         int x;
         String nome;
-        System.out.print("Digite o seu Nome: ");
+        System.out.println("Digite o seu Nome: ");
 		nome = ler.nextline(); 
+        System.out.println("Ola " + nome);
     }
     
 }
+
